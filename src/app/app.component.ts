dcd484531c5ab2e840e75d1bca9dd6e920672f8c
Ifazbeig <ifazbeighf@gmail.com>
2022-09-17 13:29:19 +0530

    first commit - csv to table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csv';
+  public tableData: any;
+  public tableTitle: any;
+  public customPagination = 1;
+  public recordsPerPage = 4;
+  public tableRecords = [];
+  public pageStartCount = 0;
+  public pageEndCount = 10;
+  public totalPageCount = 0;
+  public currentPage = 0;
+
+  public uploadData(e:any) {
+    console.log(e.target.files[0]);
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(<unknown>e.target);
+    if (target.files.length !== 1) {
+      throw new Error('Cannot use multiple files');
+    }
+
+    const reader: FileReader = new FileReader();
+    reader.readAsBinaryString(target.files[0]);
+    reader.onload = (e: any) => {
+      /* create workbook */
+      const binarystr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(binarystr, { type: 'binary' });
+
+      /* selected the first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      const data = XLSX.utils.sheet_to_json(ws); // to get 2d array pass 2nd parameter as object {header: 1}
+      console.log(data.length); // Data will be logged in array format containing objects
+      this.tableData = data;
+      this.tableTitle = Object.keys(this.tableData[0]);
+      this.tableTitle.pop();
+      this.tableTitle.shift();
+      this.tableRecords = this.tableData.slice(
+        this.pageStartCount,
+        this.pageEndCount
+      );
+      this.totalPageCount = this.tableData.length / this.recordsPerPage;
+    };
+  }
+
+  onPageChange() {
+    this.pageStartCount = this.currentPage * this.recordsPerPage;
+    this.pageEndCount = this.pageStartCount + this.recordsPerPage;
+    this.tableRecords = this.tableData.slice(
+      this.pageStartCount,
+      this.pageEndCount
+    );
+  }
 }
